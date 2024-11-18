4451d5efa1768a867b138c465fa4b97d6fc78dec
Yuki.kaco <a0936660023@gmail.com>
2024-11-18 15:20:33 +0800

    Init

--- Source/Log.cs
+++ Source/Log.cs
@@ -1,6 +1,6 @@
 using BepInEx.Logging;
 
-namespace ExampleMod;
+namespace Multiplayer;
 
 internal static class Log {
     private static ManualLogSource? logSource;
---
+++ Source/Multiplayer.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using NineSolsAPI;
+using UnityEngine;
+
+namespace Multiplayer;
+
+[BepInDependency(NineSolsAPICore.PluginGUID)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Multiplayer : BaseUnityPlugin {
+    private ConfigEntry<bool> enableSomethingConfig = null!;
+    private ConfigEntry<KeyboardShortcut> somethingKeyboardShortcut = null!;
+
+    private Harmony harmony = null!;
+
+    private void Awake() {
+        Log.Init(Logger);
+        RCGLifeCycle.DontDestroyForever(gameObject);
+
+        harmony = Harmony.CreateAndPatchAll(typeof(Multiplayer).Assembly);
+
+        enableSomethingConfig = Config.Bind("General.Something", "Enable", true, "Enable the thing");
+        somethingKeyboardShortcut = Config.Bind("General.Something", "Shortcut",
+            new KeyboardShortcut(KeyCode.H, KeyCode.LeftControl), "Shortcut to execute");
+
+        KeybindManager.Add(this, TestMethod, () => somethingKeyboardShortcut.Value);
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    private void TestMethod() {
+        ToastManager.Toast("Shortcut activated");
+
+    }
+
+    private void OnDestroy() {
+
+        harmony.UnpatchSelf();
+    }
+}
