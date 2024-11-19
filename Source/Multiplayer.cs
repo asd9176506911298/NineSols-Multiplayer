4015404aad7c06b32800ddb980729bca7dfcc186
Yuki.kaco <a0936660023@gmail.com>
2024-11-19 16:38:41 +0800

    Update

--- Source/Multiplayer.cs
+++ Source/Multiplayer.cs
@@ -19,14 +19,10 @@ public class Multiplayer : BaseUnityPlugin {
     private Harmony harmony = null!;
     private NetManager? netManager;
     private NetDataWriter dataWriter = new();
-
     private Dictionary<NetPeer, PlayerData> serverPlayers = new Dictionary<NetPeer, PlayerData>();
 
     private EventBasedNetListener? listener;
     private NetPeer? serverPeer;
-
-    [SerializeField]
-    private GameObject PlayerPrefab = null!; // Assign in Unity Editor or load dynamically
 
     private GameObject? instantiatedObject;
 
@@ -47,31 +43,6 @@ public class Multiplayer : BaseUnityPlugin {
         Log.Info($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
 
-    private void UpdatePlayerPosition(NetPeer peer, Vector3 position) {
-        if (!serverPlayers.ContainsKey(peer)) {
-            // Create a new player object
-            GameObject newPlayerObject = Instantiate(PlayerPrefab); // Make sure this is assigned
-            serverPlayers[peer] = new PlayerData(position, newPlayerObject);
-        }
-
-        serverPlayers[peer].Position = position;
-        BroadcastPlayerPosition(peer, position);
-    }
-
-    private void OnConnectedToServer() {
-        ToastManager.Toast("OnConnectedToServer");
-        if(instantiatedObject == null) {
-            GameObject spriteHolder = Player.i.transform.Find("RotateProxy").Find("SpriteHolder").gameObject;
-            instantiatedObject = Instantiate(spriteHolder);
-        }   
-    }
-
-    private void UpdatePlayerPositionOnClient(Vector3 newPosition) {
-        if (instantiatedObject != null) {
-            instantiatedObject.transform.position = newPosition;
-        }
-    }
-
     private void InitializeNetworking() {
         listener = new EventBasedNetListener();
         netManager = new NetManager(listener) { AutoRecycle = true };
@@ -79,6 +50,7 @@ public class Multiplayer : BaseUnityPlugin {
         if (isServer.Value) {
             ToastManager.Toast("StartServer");
             netManager.Start(9050);
+
             listener.ConnectionRequestEvent += request => {
                 if (netManager.ConnectedPeersCount < 10)
                     request.AcceptIfKey("game_key");
@@ -86,75 +58,175 @@ public class Multiplayer : BaseUnityPlugin {
                     request.Reject();
             };
 
+            listener.PeerConnectedEvent += peer => {
+                ToastManager.Toast($"Client connected: {peer.RemoteId}");
+
+                // Send existing players to the new client
+                foreach (var existingPlayer in serverPlayers) {
+                    NetPeer existingPeer = existingPlayer.Key;
+                    Vector3 position = existingPlayer.Value.Position;
+
+                    dataWriter.Reset();
+                    dataWriter.Put("ExistingPlayer");
+                    dataWriter.Put(existingPeer.Id); // Unique ID of the existing player
+                    dataWriter.Put(position.x);
+                    dataWriter.Put(position.y);
+                    dataWriter.Put(position.z);
+
+                    peer.Send(dataWriter, DeliveryMethod.ReliableOrdered);
+                }
+
+                // Add the new client to the player dictionary and broadcast its position
+                Vector3 spawnPosition = Player.i.transform.position;
+                UpdatePlayerPosition(peer, spawnPosition);
+                BroadcastNewPlayer(peer, spawnPosition);
+            };
+
+
+
             listener.NetworkReceiveEvent += (peer, reader, channelNumber, deliveryMethod) => {
                 float x = reader.GetFloat();
                 float y = reader.GetFloat();
                 float z = reader.GetFloat();
                 Vector3 newPosition = new Vector3(x, y, z);
-                ToastManager.Toast(newPosition);
                 UpdatePlayerPosition(peer, newPosition);
             };
         } else {
-            ToastManager.Toast("Coeenct");
+            ToastManager.Toast("Connecting to server");
             ConnectToServer();
         }
     }
 
-    private void HandleClientMessage(string message) {
-        ToastManager.Toast($"Message from server: {message}");
+
+    private void UpdatePlayerPosition(NetPeer peer, Vector3 position) {
+        if (!serverPlayers.ContainsKey(peer)) {
+            // Create a new player object if it doesn't exist
+            //GameObject newPlayerObject = Instantiate(Player.i.transform.Find("RotateProxy").Find("SpriteHolder").gameObject);
+            GameObject newPlayerObject = new GameObject("Player");
+            serverPlayers[peer] = new PlayerData(position, newPlayerObject);
+        }
+
+        serverPlayers[peer].Position = position; // Update the position for this player
+
+        // Broadcast the updated position to all clients (except the sender)
+        BroadcastPlayerPosition(peer, position);
     }
 
 
-    private void ConnectToServer() {
-        listener.NetworkReceiveEvent += (peer, reader, channelNumber, deliveryMethod) => {
-            //string message = reader.GetString();
-            //HandleClientMessage(message);
-            float x = reader.GetFloat();
-            float y = reader.GetFloat();
-            float z = reader.GetFloat();
-            Vector3 newPosition = new Vector3(x, y, z);
-            ToastManager.Toast(newPosition);
-        };
+    private void BroadcastNewPlayer(NetPeer newPeer, Vector3 position) {
+        if (netManager == null) return;
 
-        netManager.Start();
-        serverPeer = netManager.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050), "game_key");
-        OnConnectedToServer();
+        dataWriter.Reset();
+        dataWriter.Put("NewPlayer"); // Identifier for a new player
+        dataWriter.Put(position.x);
+        dataWriter.Put(position.y);
+        dataWriter.Put(position.z);
+
+        foreach (var peer in netManager.ConnectedPeerList) {
+            if (peer != newPeer) { // Exclude the new client
+                peer.Send(dataWriter, DeliveryMethod.ReliableOrdered);
+            }
+        }
     }
 
     private void BroadcastPlayerPosition(NetPeer excludePeer, Vector3 position) {
         if (netManager == null) return;
 
         dataWriter.Reset();
+        dataWriter.Put("PlayerPosition"); // Identifier for player position
         dataWriter.Put(position.x);
         dataWriter.Put(position.y);
         dataWriter.Put(position.z);
 
+        // Broadcast the updated position to all peers except the one sending the update
         foreach (var peer in netManager.ConnectedPeerList) {
-            if (peer != excludePeer) {
+            if (peer != excludePeer) { // Exclude the sender
                 peer.Send(dataWriter, DeliveryMethod.ReliableOrdered);
             }
+        }
+    }
+
+    private void ConnectToServer() {
+        listener.NetworkReceiveEvent += (peer, reader, channelNumber, deliveryMethod) => {
+            string messageType = reader.GetString();
+
+            if (messageType == "NewPlayer") {
+                float x = reader.GetFloat();
+                float y = reader.GetFloat();
+                float z = reader.GetFloat();
+                Vector3 newPlayerPosition = new Vector3(x, y, z);
+                ToastManager.Toast($"New player joined at position {newPlayerPosition}");
+                UpdatePlayerPositionOnClient(newPlayerPosition);
+            } else if (messageType == "PlayerPosition") {
+                float x = reader.GetFloat();
+                float y = reader.GetFloat();
+                float z = reader.GetFloat();
+                Vector3 updatedPosition = new Vector3(x, y, z);
+                UpdatePlayerPositionOnClient(updatedPosition);
+            } else if (messageType == "ExistingPlayer") {
+                int playerId = reader.GetInt();
+                float x = reader.GetFloat();
+                float y = reader.GetFloat();
+                float z = reader.GetFloat();
+                Vector3 existingPlayerPosition = new Vector3(x, y, z);
+                ToastManager.Toast($"Existing player {playerId} at position {existingPlayerPosition}");
+                UpdatePlayerPositionOnClient(existingPlayerPosition);
+            }
+        };
+
+        netManager.Start();
+        serverPeer = netManager.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050), "game_key");
+    }
+
+
+    //private void CreatePlayer() {
+    //    ToastManager.Toast("Connected to server");
+    //    if (instantiatedObject == null) {
+    //        GameObject spriteHolder = Player.i.transform.Find("RotateProxy").Find("SpriteHolder").gameObject;
+    //        instantiatedObject = Instantiate(spriteHolder);
+    //    }
+    //}
+
+    private void UpdatePlayerPositionOnClient(Vector3 newPosition) {
+        if (instantiatedObject != null) {
+            instantiatedObject.transform.position = newPosition;
+        }
+    }
+
+    private void TestMethod() {
+        if (netManager != null) {
+            ToastManager.Toast("Sending position");
+            SendPlayerPositionToServer(new Vector3(1f, 2f, 3f));
         }
     }
 
     private void SendPlayerPositionToServer(Vector3 position) {
         if (netManager != null && netManager.FirstPeer != null) {
             dataWriter.Reset();
+            dataWriter.Put("PlayerPosition");
             dataWriter.Put(position.x);
             dataWriter.Put(position.y);
             dataWriter.Put(position.z);
+
             netManager.FirstPeer.Send(dataWriter, DeliveryMethod.ReliableOrdered);
         }
     }
 
-    private void TestMethod() {
-        if (netManager != null) {
-            ToastManager.Toast("123");
-            SendPlayerPositionToServer(new Vector3(1f,2f,3f));
-        }
+    private void Update() {
+        //if (Player.i != null) {
+        //    Vector3 newPosition = Player.i.transform.position;
+        //    // Send the updated position to the server if it's different from the current one
+        //    if (HasPositionChanged(newPosition)) {
+        //        SendPlayerPositionToServer(newPosition);
+        //    }
+        //}
+
+        netManager?.PollEvents();
     }
 
-    private void Update() {
-        netManager?.PollEvents();
+    private bool HasPositionChanged(Vector3 newPosition) {
+        // Compare with the last position stored for the player
+        return Player.i.transform.position != newPosition;
     }
 
     private void OnDestroy() {
