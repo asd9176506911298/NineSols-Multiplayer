1171da69fbc72a2bb236f439bdee2f7275a2aeaf
Yuki.kaco <a0936660023@gmail.com>
2024-11-21 12:38:01 +0800

    Update

---
+++ Server/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Server
+{
+    internal class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
