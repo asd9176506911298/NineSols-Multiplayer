20a8faa6ed7d2d88c5851738923d0090a306abf0
Yuki.kaco <a0936660023@gmail.com>
2024-11-21 12:49:44 +0800

    Update

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LiteNetLib;
+using LiteNetLib.Utils;
+using System;
+using System.Threading;
 
 namespace Server
 {
@@ -7,7 +10,32 @@ namespace Server
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.Read();
+
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(9050 /* port */);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if (server.ConnectedPeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer);  // Show peer ip
+                NetDataWriter writer = new NetDataWriter();         // Create writer class
+                writer.Put("Hello client!");                        // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);  // Send with reliability
+            };
+
+            while (!Console.KeyAvailable) {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+            server.Stop();
         }
     }
 }
